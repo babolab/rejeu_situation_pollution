5dcad3067c5d0d189d89a4c7e02e9ad340b4764f
babolab <geoffrey.babault@gmail.com>
2025-02-25 09:51:02 +0100

    modified:   app.py

--- app.py
+++ app.py
@@ -1,35 +1,27 @@
 import streamlit as st
+import io
 import os
 import base64
 from generate_gif import extract_gpx_points, save_map_images, create_gif
 from datetime import datetime
 
 def main():
-    st.title("Projection de simulation de dérive et trajectoires de navires")
+    st.title("Simulation de Dérive et Trajectoires de Navires")
 
     # Upload files
     derive_file = st.file_uploader("Télécharger le fichier de dérive", type=["gpx"])
-    trajectoire_files = st.file_uploader("Télécharger les fichiers de trajectoires de navires faits avec VTS", type=["gpx"], accept_multiple_files=True)
+    trajectoire_files = st.file_uploader("Télécharger les fichiers de trajectoire", type=["gpx"], accept_multiple_files=True)
 
     colors = ['red', 'green', 'blue', 'purple', 'orange', 'darkred', 'lightred', 'beige', 'darkblue', 'darkgreen', 'cadetblue', 'darkpurple', 'pink', 'lightblue', 'lightgreen']
 
     if st.button("Lancer la simulation"):
         if derive_file and trajectoire_files:
-            # Save uploaded files to disk
-            derive_path = os.path.join("mothy", derive_file.name)
-            with open(derive_path, "wb") as f:
-                f.write(derive_file.getbuffer())
+            # Create list of trajectory file names
+            trajectoire_paths = [file.name for file in trajectoire_files]
 
-            trajectoire_paths = []
-            for file in trajectoire_files:
-                path = os.path.join("trajectoires", file.name)
-                with open(path, "wb") as f:
-                    f.write(file.getbuffer())
-                trajectoire_paths.append(path)
-
-            # Process files
-            derive_points = extract_gpx_points(derive_path)
-            trajectoire_points_list = [extract_gpx_points(path) for path in trajectoire_paths]
+            # Process files in memory
+            derive_points = extract_gpx_points(io.BytesIO(derive_file.getbuffer()))
+            trajectoire_points_list = [extract_gpx_points(io.BytesIO(file.getbuffer())) for file in trajectoire_files]
 
             start_time = min(point[2] for point in derive_points if point[2] is not None)
             end_time = max(max(point[2] for point in points if point[2] is not None) for points in trajectoire_points_list)
